31e1da5b309eb9db2a6fa88b3fcc2ebdee3a3f27
Ahmad Jailani <32035123+ajailani4@users.noreply.github.com>
2023-03-31 20:53:36 +0700

    Update BaseResponse and RegisterRequest

--- app/src/main/java/com/sosyal/app/data/remote/dto/request/RegisterRequest.kt
+++ app/src/main/java/com/sosyal/app/data/remote/dto/request/RegisterRequest.kt
@@ -1,5 +1,8 @@
 package com.sosyal.app.data.remote.dto.request
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class RegisterRequest(
     val name: String,
     val email: String,
--- app/src/main/java/com/sosyal/app/data/remote/dto/response/BaseResponse.kt
+++ app/src/main/java/com/sosyal/app/data/remote/dto/response/BaseResponse.kt
@@ -1,7 +1,6 @@
 package com.sosyal.app.data.remote.dto.response
 
 data class BaseResponse<T>(
-    val code: Int,
-    val status: String,
-    val data: T
+    val message: String,
+    val data: T? = null
 )
