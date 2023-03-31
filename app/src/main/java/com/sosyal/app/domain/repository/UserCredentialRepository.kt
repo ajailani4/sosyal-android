657a6b3f227c8cf1fc6a67cacfa3ee4fc4863ab6
Ahmad Jailani <32035123+ajailani4@users.noreply.github.com>
2023-03-31 18:48:46 +0700

    Add domain layer for auth feature

--- app/src/main/java/com/sosyal/app/domain/repository/UserCredentialRepository.kt
+++ app/src/main/java/com/sosyal/app/domain/repository/UserCredentialRepository.kt
@@ -1,4 +1,9 @@
 package com.sosyal.app.domain.repository
 
+import kotlinx.coroutines.flow.Flow
+
 interface UserCredentialRepository {
+    suspend fun saveAccessToken(accessToken: String)
+
+    fun getAccessToken(): Flow<String>
 }
