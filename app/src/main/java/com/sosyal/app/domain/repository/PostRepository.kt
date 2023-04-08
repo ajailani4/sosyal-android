95699a2d3754677c4b0719f4adb071dbea3a9cf4
Ahmad Jailani <32035123+ajailani4@users.noreply.github.com>
2023-04-08 12:56:47 +0700

    Add getPostDetail function in domain layer of post feature

--- app/src/main/java/com/sosyal/app/domain/repository/PostRepository.kt
+++ app/src/main/java/com/sosyal/app/domain/repository/PostRepository.kt
@@ -1,10 +1,14 @@
 package com.sosyal.app.domain.repository
 
 import com.sosyal.app.domain.model.Post
+import com.sosyal.app.util.Resource
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharedFlow
 
 interface PostRepository {
     fun receivePost(): SharedFlow<Post>
 
     suspend fun uploadPost(post: Post)
+    
+    fun getPostDetail(): Flow<Resource<Post>>
 }
