97400a38694ca33622251090e9f282c07a4c7aaf
Ahmad Jailani <32035123+ajailani4@users.noreply.github.com>
2023-04-09 20:56:57 +0700

    Update casting of getUserProfile response in UserProfileRepositoryImpl

--- app/src/main/java/com/sosyal/app/data/repository/UserProfileRepositoryImpl.kt
+++ app/src/main/java/com/sosyal/app/data/repository/UserProfileRepositoryImpl.kt
@@ -5,6 +5,7 @@ import com.sosyal.app.R
 import com.sosyal.app.data.mapper.toUserProfile
 import com.sosyal.app.data.remote.data_source.UserProfileRemoteDataSource
 import com.sosyal.app.data.remote.dto.UserProfileDto
+import com.sosyal.app.data.remote.dto.response.BaseResponse
 import com.sosyal.app.domain.repository.UserProfileRepository
 import com.sosyal.app.util.Resource
 import io.ktor.client.call.*
@@ -21,8 +22,8 @@ class UserProfileRepositoryImpl(
 
             when (response.status) {
                 HttpStatusCode.OK -> {
-                    val responseBody = response.body() as UserProfileDto
-                    emit(Resource.Success(responseBody.toUserProfile()))
+                    val responseBody = response.body() as BaseResponse<UserProfileDto>
+                    emit(Resource.Success(responseBody.data?.toUserProfile()))
                 }
 
                 HttpStatusCode.InternalServerError -> emit(Resource.Error(context.getString(R.string.server_error)))
