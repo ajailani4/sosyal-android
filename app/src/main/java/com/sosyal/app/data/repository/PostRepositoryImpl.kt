0c4d1a80e3ab6637750ecfbc3adb3a2f34a9d868
Ahmad Jailani <32035123+ajailani4@users.noreply.github.com>
2023-04-09 11:48:04 +0700

    Change Resource type of deletePost function in PostRepository

--- app/src/main/java/com/sosyal/app/data/repository/PostRepositoryImpl.kt
+++ app/src/main/java/com/sosyal/app/data/repository/PostRepositoryImpl.kt
@@ -14,6 +14,7 @@ import io.ktor.client.call.*
 import io.ktor.http.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.serialization.json.JsonObject
 
 class PostRepositoryImpl(
     private val postRemoteDataSource: PostRemoteDataSource,
@@ -47,7 +48,7 @@ class PostRepositoryImpl(
 
             when (response.status) {
                 HttpStatusCode.OK -> {
-                    val responseBody = response.body() as BaseResponse<Any>
+                    val responseBody = response.body() as BaseResponse<JsonObject>
                     emit(Resource.Success(responseBody.data))
                 }
 
--- app/src/main/java/com/sosyal/app/domain/repository/PostRepository.kt
+++ app/src/main/java/com/sosyal/app/domain/repository/PostRepository.kt
@@ -4,6 +4,7 @@ import com.sosyal.app.domain.model.Post
 import com.sosyal.app.util.Resource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.serialization.json.JsonObject
 
 interface PostRepository {
     fun receivePost(): SharedFlow<Post>
@@ -12,5 +13,5 @@ interface PostRepository {
 
     fun getPostDetail(id: String): Flow<Resource<Post>>
 
-    fun deletePost(id: String): Flow<Resource<Any>>
+    fun deletePost(id: String): Flow<Resource<JsonObject>>
 }
