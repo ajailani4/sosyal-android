cd8945f080dc3acb88e74efe664027c85cdc8e7f
Ahmad Jailani <32035123+ajailani4@users.noreply.github.com>
2023-04-12 21:32:09 +0700

    Add edit profile function in data layer of user profile feature

--- app/src/main/java/com/sosyal/app/data/remote/api_service/UserProfileService.kt
+++ app/src/main/java/com/sosyal/app/data/remote/api_service/UserProfileService.kt
@@ -2,7 +2,28 @@ package com.sosyal.app.data.remote.api_service
 
 import io.ktor.client.*
 import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.http.*
+import java.io.File
 
 class UserProfileService(private val httpClient: HttpClient) {
     suspend fun getUserProfile() = httpClient.get("/profile")
+    suspend fun editUserProfile(
+        name: String,
+        email: String,
+        avatar: File?
+    ) =
+        httpClient.submitFormWithBinaryData(
+            url = "/profile",
+            formData = formData {
+                append("name", name)
+                append("email", email)
+
+                avatar?.readBytes()?.let {
+                    append("avatar", it, Headers.build {
+                        append(HttpHeaders.ContentType, "image/*")
+                    })
+                }
+            }
+        )
 }
--- app/src/main/java/com/sosyal/app/data/repository/UserProfileRepositoryImpl.kt
+++ app/src/main/java/com/sosyal/app/data/repository/UserProfileRepositoryImpl.kt
@@ -10,7 +10,10 @@ import com.sosyal.app.domain.repository.UserProfileRepository
 import com.sosyal.app.util.Resource
 import io.ktor.client.call.*
 import io.ktor.http.*
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.serialization.json.JsonObject
+import java.io.File
 
 class UserProfileRepositoryImpl(
     private val userProfileRemoteDataSource: UserProfileRemoteDataSource,
@@ -24,6 +27,30 @@ class UserProfileRepositoryImpl(
                 HttpStatusCode.OK -> {
                     val responseBody = response.body() as BaseResponse<UserProfileDto>
                     emit(Resource.Success(responseBody.data?.toUserProfile()))
+                }
+
+                HttpStatusCode.InternalServerError -> emit(Resource.Error(context.getString(R.string.server_error)))
+
+                else -> emit(Resource.Error(context.getString(R.string.something_wrong_happened)))
+            }
+        }
+
+    override fun editUserProfile(
+        name: String,
+        email: String,
+        avatar: File?
+    ) =
+        flow {
+            val response = userProfileRemoteDataSource.editUserProfile(
+                name = name,
+                email = email,
+                avatar = avatar
+            )
+
+            when (response.status) {
+                HttpStatusCode.OK -> {
+                    val responseBody = response.body() as BaseResponse<JsonObject>
+                    emit(Resource.Success(responseBody.data))
                 }
 
                 HttpStatusCode.InternalServerError -> emit(Resource.Error(context.getString(R.string.server_error)))
