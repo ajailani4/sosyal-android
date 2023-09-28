c64e7aab20d0db001642bca24ac3bc4179b3e483
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-09-28 13:17:41 +0700

    Update GetChatsUseCase

--- app/src/main/java/com/sosyal/app/domain/use_case/chat/GetChatsUseCase.kt
+++ app/src/main/java/com/sosyal/app/domain/use_case/chat/GetChatsUseCase.kt
@@ -3,7 +3,5 @@ package com.sosyal.app.domain.use_case.chat
 import com.sosyal.app.domain.repository.ChatRepository
 
 class GetChatsUseCase(private val chatRepository: ChatRepository) {
-    operator fun invoke() {
-        chatRepository.getChats()
-    }
+    operator fun invoke() = chatRepository.getChats()
 }
